d8a579d8226bf281e9839c92cdbd6bfa3fc1cd65
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-25 13:37:40 +0900

    backup

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 # 파일 빌드
 g++ -o ./build/main ./src/main.cpp
-# test 1 검사
-./build/main < ./testcase/input_test1.txt  > output_test1.txt
-diff ./testcase/answer_test1.txt output_test1.txt
-# test 2 검사
-./build/main < ./testcase/input_test2.txt  > output_test2.txt
-diff ./testcase/answer_test2.txt output_test2.txt
-# test 3 검사
-./build/main < ./testcase/input_test3.txt  > output_test3.txt
-diff ./testcase/answer_test3.txt output_test3.txt
+
+./build/main
